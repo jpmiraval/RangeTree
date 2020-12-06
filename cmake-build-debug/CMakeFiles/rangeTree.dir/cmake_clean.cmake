23794400d236d151b1a2b6e51fdc257dbf183f02
jpmiraval <40766473+jpmiraval@users.noreply.github.com>
2020-12-05 23:01:40 -0500

    DONE

---
+++ cmake-build-debug/CMakeFiles/rangeTree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rangeTree.dir/Experimentos.cpp.o"
+  "rangeTree"
+  "rangeTree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rangeTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
